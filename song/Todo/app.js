fa600fe387ee0fef48886fc94680a8df58e848d9
Pisodev77 <pisodev0427@gmail.com>
2024-02-23 11:24:22 +0900

    feat: root 노드에 innerHTML 추가
    - root에 내용을 추가합니다.

--- song/Todo/app.js
+++ song/Todo/app.js
@@ -1,4 +1,7 @@
 const $root = document.getElementById('root');
 
+$root.innerHTML = `
+	<h1>변경될 수 있는 태그의 변경 전 모습입니다.</h1>
+`;
 // DOMContentLoaded는 HTML 문서 완전히 파싱(해석) 되었을때 실행됩니다.
 window.addEventListener('DOMContentLoaded', () => {});
