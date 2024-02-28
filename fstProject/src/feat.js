593b5be9ee21ee288679e65c5796c49bf134b43c
Pisodev77 <pisodev0427@gmail.com>
2024-02-28 15:30:28 +0900

    feat: DOMString 방식으로 데이터 송수신

--- fstProject/src/feat.js
+++ fstProject/src/feat.js
@@ -1,1 +1,5 @@
-export const myVariable = 100;
+export default `
+<h1 class="title">지문 쏼라쏼라 기능</h1>
+<label class="textareaLabel">한글로부터 텍스트 복붙하는 TA</label>
+<textarea id="textareaId" placeholder="여따 적어라"></textarea>
+`;
