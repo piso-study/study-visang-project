dab35c81f705d5386dac7ac0b462630251d1e69f
hong <reach9326@naver.com>
2024-03-08 11:43:02 +0900

    feat: article.js의 module을 import하고, EventListener에 변환 기능 추가

--- fstProject/app.js
+++ fstProject/app.js
@@ -1,15 +1,23 @@
 import DOMStringJimoon from './src/feat.js';
+import DOMStringArticle from './src/article.js';
+import { attrsExtract } from './src/article.js';
 
 const pages = {
-	tmp: DOMStringJimoon,
+    tmp: DOMStringJimoon,
+    article: DOMStringArticle,
 };
 
 const $root = document.getElementById('root');
 
 document.querySelector('header').addEventListener('click', (event) => {
-	if (event.target.classList.contains('pageLink')) {
-		const { link } = event.target.dataset;
-		if (!link) return;
-		$root.innerHTML = pages[link];
-	}
+    if (event.target.classList.contains('pageLink')) {
+        const { link } = event.target.dataset;
+        if (!link) return;
+
+        $root.innerHTML = pages[link];
+
+        if (link === 'article') {
+            attrsExtract();
+        }
+    }
 });
