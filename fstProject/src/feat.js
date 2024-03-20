cee538b6d74489f455eaa36afb6e4d418cc647f9
Pisodev77 <pisodev0427@gmail.com>
2024-03-20 16:55:44 +0900

    feat: {콘텐츠} 별로 발문과 지문 나누는 기능 구현

--- fstProject/src/feat.js
+++ fstProject/src/feat.js
@@ -2,4 +2,31 @@ export default `
 <h1 class="title">지문 쏼라쏼라 기능</h1>
 <label class="textareaLabel">한글로부터 텍스트 복붙하는 TA</label>
 <textarea id="textareaId" placeholder="여따 적어라"></textarea>
+<button id="tmpBtn">my btn</button>
+<textarea id="res"></textarea>
 `;
+
+const func = () => {
+	let contents = document.getElementById('textareaId').value.split('{콘텐츠}');
+	const arr = [];
+
+	const types = [
+		{ typeStr: '{발문}', className: 'question' },
+		{ typeStr: '{지문}', className: 'jimoon' },
+	];
+
+	types.forEach(({ typeStr, className }) => {
+		contents = contents.map((content) => {
+			const [question, rest] = content.split(typeStr);
+			arr.push(`<div class="${className}">\n\t${question.trim()}\n</div>`);
+			return `${rest}`;
+		});
+	});
+
+	return arr.join('\n');
+};
+
+document.getElementById('root').addEventListener('click', (e) => {
+	if (e.target.id !== 'tmpBtn') return;
+	document.getElementById('res').textContent = func();
+});
